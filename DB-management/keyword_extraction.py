e9c67e960a6819fe09ef9bf76e5af487f0ee7b32
moon <larcane@naver.com>
2022-08-02 10:03:32 +0000

    [feat] add code to keyword extraction
    - This code is to refresh tag table in DB when auto tagging model is changed.
    - First, this code truncate tag table, tag_has_music, user_has_tag table in DB
    - And then, re-create tag and re-alloc tag_has_music using new auto tag model
    - This code don't re-alloc user_has_music. this part is to be updated soon.

---
+++ DB-management/keyword_extraction.py
@@ -0,0 +1,48 @@
+"""
+새로운 태깅 모델을 사용해 DB의 태그를 모두 수정할 때 사용
+
+1. 기존 DB의 tag 데이터를 모두 삭제
+2. 태그 모델에서 각 노래에 대한 태그를 추출 후 새로 저장
+
+"""
+import os
+import requests
+from tqdm import tqdm
+import json
+from utils import get_lyric_by_musicDB,get_all_music_id_list
+from utils import save_tag_list_in_db
+from utils import init_tag_table_in_db
+
+assert os.environ.get("DB_HOST") is not None, "should register DB account in environment variables."
+assert os.environ.get("DB_USER") is not None, "should register DB account in environment variables."
+assert os.environ.get("DB_NAME") is not None, "should register DB account in environment variables."
+assert os.environ.get("DB_PASSWORD") is not None, "should register DB account in environment variables."
+
+
+host = os.environ.get("DB_HOST")
+user = os.environ.get("DB_USER")
+db = os.environ.get("DB_NAME")
+password = os.environ.get("DB_PASSWORD")
+
+print("init tag table..")
+init_tag_table_in_db(host,user,db,password)
+print("init tag table finish.")
+
+print("start extraction tag by music id..")
+all_music_id_list = get_all_music_id_list(host,user,db,password)
+tag_dict={}
+for music_id in tqdm(all_music_id_list):
+    lyric = get_lyric_by_musicDB(music_id,host,user,db,password)
+    params = {"musicLyric":lyric}
+    tag_list = requests.post(f"http://10.1.3.111:5000/music/tag/extraction",json=params).json()['tagList']
+    tag_dict[music_id]=tag_list
+    save_tag_list_in_db(tag_list,music_id,host,user,db,password)
+
+with open("./tag_dict.json","w") as f:
+    json.dump(tag_dict,f)
+print("tag dict is saved in ./tag_dict.json")
+
+print("No keyword list :")
+for k,v in tag_dict.items():
+    if len(v)==0:
+        print("\t music id : ",k)
---
+++ DB-management/utils.py
@@ -0,0 +1,48 @@
+import pymysql
+from pymysql.constants import CLIENT
+
+def init_tag_table_in_db(host,user,db,password):
+    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8',client_flag=CLIENT.MULTI_STATEMENTS)
+    with db:
+        with db.cursor() as cursor:
+            sql = """
+                TRUNCATE TABLE tag_has_music;
+                TRUNCATE TABLE user_has_tag;
+                SET FOREIGN_KEY_CHECKS = 0; -- Disable foreign key checking.
+                TRUNCATE TABLE tag;
+                SET FOREIGN_KEY_CHECKS = 1; -- Enable foreign key checking.
+                """
+            cursor.execute(sql)
+
+def get_lyric_by_musicDB(musicid,host,user,db,password):
+    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8',client_flag=CLIENT.MULTI_STATEMENTS)
+    with db:
+        with db.cursor() as cursor:
+            sql = """SELECT lyric FROM music WHERE id=%s"""
+            cursor.execute(sql,(musicid))
+            lyric = cursor.fetchone()[0]
+    return lyric
+    
+def get_all_music_id_list(host,user,db,password):
+    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8')
+    sql = "SELECT id FROM music"
+    with db:
+        with db.cursor() as cursor:
+            cursor.execute(sql)
+            all_music_id_list = [music_id[0] for music_id in cursor.fetchall()]
+    return all_music_id_list
+
+def save_tag_list_in_db(tag_list,musicid,host,user,db,password):
+    db = pymysql.connect(host=host,user=user,db=db,password=password,charset='utf8',client_flag=CLIENT.MULTI_STATEMENTS)
+    with db:
+        with db.cursor() as cursor:
+            delete_prev_tag_sql = """DELETE FROM tag_has_music WHERE music_id=%s"""
+            cursor.execute(delete_prev_tag_sql,(musicid))
+            
+            tag_has_music_insert_sql = """INSERT INTO tag_has_music(tag_id,music_id,tag_rank)
+                                                VALUES((SELECT id FROM tag WHERE name=%s),%s,%s)"""
+            tag_insert_sql = """INSERT IGNORE INTO tag(name) VALUES(%s)"""
+            for rank,tag in enumerate(tag_list):
+                cursor.execute(tag_insert_sql,(tag))
+                cursor.execute(tag_has_music_insert_sql,(tag,musicid,rank+1))
+        db.commit()
